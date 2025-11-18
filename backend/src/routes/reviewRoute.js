926da86450e74760dc9e0e559284bfe6d537b38d
ilcubo <sasp3name16f@gmail.com>
2025-11-18 20:06:34 +0700

    feat: add endpoint for creating reviews for logged in users

--- backend/src/routes/reviewRoute.js
+++ backend/src/routes/reviewRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
+import {verifyToken} from "../middleware/authMiddleware.js";
 
 import * as reviewController from "../controllers/reviewController.js"
 
 const router = express.Router();
 
 router.get("/:movie", reviewController.getReviews);
+router.post("/", verifyToken, reviewController.createReview);
 
 export default router;
