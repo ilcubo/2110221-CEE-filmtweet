d19d841bf557c3c90375a41c1b527a6c2c7e6a95
ilcubo <sasp3name16f@gmail.com>
2025-11-19 12:28:43 +0700

    fix: make the hamburger menu toggles peoperly and close when the screen is resized to a larger size

--- frontend/public/scripts/app.js
+++ frontend/public/scripts/app.js
@@ -1,7 +1,10 @@
+const navbarToggle = document.getElementById("navbar-toggle");
+const navbarMobile = document.getElementById("navbar-mobile-menu");
 const searchButton = document.getElementById("floating-search-btn");
 const searchDialog = document.getElementById("search-dialog");
 
 var searchDialogVisible = false;
+var navbarMobileVisible = false;
 
 searchButton.addEventListener("click", () => {
   if (searchDialogVisible) {
@@ -10,5 +13,22 @@ searchButton.addEventListener("click", () => {
   } else {
     searchDialog.style = "display: block; position: static;";
     searchDialogVisible = true;
+  }
+});
+
+navbarToggle.addEventListener("click", () => {
+  if (navbarMobileVisible) {
+    navbarMobile.className = "navbar-mobile-menu";
+    navbarMobileVisible = false;
+  } else {
+    navbarMobile.className = "navbar-mobile-menu.active";
+    navbarMobileVisible = true;
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 480) {
+    navbarMobile.className = "navbar-mobile-menu";
+    navbarMobileVisible = false;
   }
 });
